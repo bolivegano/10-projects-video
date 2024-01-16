58be7d826b6a38938a46574f0a52a01c74bb9625
bolivegano <bolivegano@proton.me>
2024-01-16 16:30:49 -0600

    tip calculator

---
+++ recipe-app/index.js
@@ -0,0 +1,49 @@
+const API_KEY = "803332d8a3334e228445381f6fc7fe53";
+const recipeListEl = document.getElementById("recipe-list");
+
+function displayRecipes(recipes) {
+    recipeListEl.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
+
+        const recipeImageEl = document.createElement("img");
+        recipeImageEl.src = recipe.image;
+        recipeImageEl.alt = "recipe image";
+
+        const recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText = recipe.title;
+
+        const recipeIngredientsEl = document.createElement("p");
+        recipeIngredientsEl.innerHTML = `<strong>Ingredients:</strong> ${recipe.extendedIngredients
+            .map((ingredient) => ingredient.original)
+            .join(", ")}`;
+
+        const recipeLinkEl = document.createElement("a");
+        recipeLinkEl.href = recipe.sourceUrl;
+        recipeLinkEl.innerText = "View Recipe";
+
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipeItemEl.appendChild(recipeIngredientsEl);
+        recipeItemEl.appendChild(recipeLinkEl);
+        recipeListEl.appendChild(recipeItemEl);
+    });
+}
+
+async function getRecipes() {
+    const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?number=10&include-tags=vegan&apiKey=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    return data.recipes;
+}
+
+async function init() {
+    const recipes = await getRecipes();
+    displayRecipes(recipes);
+}
+
+init();
